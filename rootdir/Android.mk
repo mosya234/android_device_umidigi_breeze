13a2e4d6391c4b1b1c4af1ec7056624c6049e381
Hadenix <vsegda_online_007@bk.ru>
2021-01-10 19:46:36 +0300

    breeze: fix build error
    - FAILED: ninja: 'device/umidigi/breeze/rootdir/etc/fstab.enableswap', needed by 'out/target/product/breeze/obj/ETC/fstab.enableswap_intermediates/fstab.enableswap', missing and no known rule to make it

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -17,19 +17,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.mt6763
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := fstab.enableswap
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.enableswap
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.safailnet.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.safailnet.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
-include $(BUILD_PREBUILT)
