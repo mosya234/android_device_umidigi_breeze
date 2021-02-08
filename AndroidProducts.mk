568e66acc127c7fb2249fd015f5613798053aca1
Hadenix <vsegda_online_007@bk.ru>
2021-02-08 17:03:23 +0300

    breeze: update COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -18,4 +18,6 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/lineage_breeze.mk
 
 COMMON_LUNCH_CHOICES += \
-    lineage_breeze-userdebug
+    lineage_breeze-user \
+    lineage_breeze-userdebug \
+    lineage_breeze-eng
